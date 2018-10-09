669183d47d0aea47c1e87c51b893deeb370221a9
Rawk-Star <jeremydalby@gmail.com>
2018-10-09 09:51:00 -0400

    Initial commit

---
+++ app/src/main/java/com/rawks/jeremy/splitthebill/MainActivity.java
@@ -0,0 +1,30 @@
+package com.rawks.jeremy.splitthebill;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity
+{
+    Button btnStart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnStart = findViewById(R.id.btnStart);
+
+        btnStart.setOnClickListener(btnStartClick);
+    }
+    Button.OnClickListener btnStartClick = new Button.OnClickListener()
+    {
+        @Override
+        public void onClick(View v)
+        {
+
+        }
+    };
+}
